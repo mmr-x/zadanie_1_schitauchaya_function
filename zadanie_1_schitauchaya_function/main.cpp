a823da9486cfb6fc15cc3576733db0e896d1f290
mmr-x <exitroom@list.ru>
2025-10-01 22:31:33 +0300

    Add project files.

---
+++ zadanie_1_schitauchaya_function/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int count = 0;
+
+void counting_function( )
+{
+    count++;
+
+    std::cout << "function called at " << count << std::endl;
+}
+
+int main( int argc, char** argv )
+{
+    for ( int i = 0; i < 15; i++ )
+    {
+        counting_function( );
+    }
+}
